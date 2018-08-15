c6bbec498576415074166b29e96af8939ec9eb14
Siavash Safi <theexxo@hotmail.com>
2018-08-15 13:43:13 +0430

    Add Admin Panel UI

---
+++ HotelPanel/Controllers/AccommodationController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HotelPanel.Controllers
+{
+    public class AccommodationController : Controller
+    {
+        // GET: Accommodation
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ HotelPanel/Controllers/LoginController.cs
@@ -0,0 +1,31 @@
+﻿using Data.PublicModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HotelPanel.Controllers
+{
+    public class LoginController : Controller
+    {
+        /// <summary>
+        /// Login View
+        /// </summary>
+        /// <returns>View Login</returns>
+        public ActionResult Index()
+        {
+            return View();
+        }
+        /// <summary>
+        /// Login Post Data
+        /// </summary>
+        /// <param name="Model">LoginEntry</param>
+        /// <returns>View Admin Panel If Login Result Success</returns>
+        [HttpPost]
+        public ActionResult Index(LoginModels.LoginEntry Model)
+        {
+            return View();
+        }
+    }
+}
