f2b9f9a288c049c6b5f141d1d5b59aa8e1333840
Matin Moezzi <matin.moezzi@outlook.com>
2018-09-02 15:26:54 +0430

    edit verifypanel controller

--- HotelPanel/Controllers/VerifyPanelController.cs
+++ HotelPanel/Controllers/VerifyPanelController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Data.ViewModel.VerifyPanelViewModels;
+using Logic.BusinessObjects;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,7 +10,33 @@ namespace HotelPanel.Controllers
 {
     public class VerifyPanelController : Controller
     {
+        private readonly VerifyPanelBusiness verifyPanelBusiness = new VerifyPanelBusiness();
         // GET: VerifyPanel
         public ActionResult Index() => View();
+
+        public JsonResult GetAccommodationInfo(long id)
+        {
+            try
+            {
+                return Json(new { success = true, data = verifyPanelBusiness.GetAccommodationInfo(id) }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                return Json(new { success = false, data = ex.Message }, JsonRequestBehavior.AllowGet);
+            }
+        }
+        public JsonResult Verify(HotelInfoViewModel model)
+        {
+            try
+            {
+                if (verifyPanelBusiness.Verify(model, out string message))
+                    return Json(new { success = true, data = message }, JsonRequestBehavior.AllowGet);
+                return Json(new { success = false, data = message }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                return Json(new { success = false, data = ex.Message }, JsonRequestBehavior.AllowGet);
+            }
+        }
     }
 }
