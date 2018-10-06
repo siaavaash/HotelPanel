e922be8e33600d33bd469347f50bdcfb527d9f1f
Matin Moezzi <matin.moezzi@outlook.com>
2018-10-06 19:14:53 +0330

    DOTWBusiness

---
+++ Logic/BusinessObjects/DOTWBusiness.cs
@@ -0,0 +1,82 @@
+﻿using Service.ServiceModel.DOTWModels;
+using Service.Suppliers;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Logic.BusinessObjects
+{
+    public class DOTWBusiness
+    {
+        public bool GetInternalCodes(Command command, string path)
+        {
+            try
+            {
+                if (DOTWAccess.GetInternalCodes(command, out string result))
+                {
+                    File.WriteAllText(path, result);
+                    return true;
+                }
+                return false;
+            }
+            catch (System.Exception)
+            {
+
+                throw;
+            }
+        }
+        public bool GetHotelsByCity(int code, string path)
+        {
+            try
+            {
+                if (DOTWAccess.SearchHotelByCity(code, out string result))
+                {
+                    File.WriteAllText(path, result);
+                    return true;
+                }
+                return false;
+            }
+            catch (System.Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public List<SearchHotelByCityViewModel> GetHotelsByAllCities(string inPath, string outPath)
+        {
+            try
+            {
+                var result = new List<SearchHotelByCityViewModel>();
+                XmlReader reader = XmlReader.Create(inPath);
+                XmlSerializer serializer = new XmlSerializer(typeof(result));
+                if (serializer.CanDeserialize(reader))
+                {
+                    var response = (result)serializer.Deserialize(reader);
+                    if (response.successful == "FALSE") return new List<SearchHotelByCityViewModel>();
+                    var cities = ((result)serializer.Deserialize(reader)).cities.city;
+                    Parallel.ForEach(cities, city =>
+                    {
+                        var success = GetHotelsByCity(city.code, outPath + city.name + ".xml");
+                        result.Add(new SearchHotelByCityViewModel
+                        {
+                            CityCode = city.code.ToString(),
+                            Success = success,
+                        });
+                    });
+                }
+                else
+                    throw new System.Exception("The File is incorrect format.");
+                return result;
+
+            }
+            catch (System.Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
---
+++ Service/Suppliers/DOTWAccess.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Suppliers
+{
+    public enum Command
+    {
+        getcurrenciesids = 1,
+        getallcities = 2,
+        getallcountries = 3,
+        getlanguagesids = 4,
+        getleisureids = 5,
+        getbusinessids = 6,
+        getamenitieids = 7,
+        getroomamenitieids = 8,
+        getsalutationsids = 9,
+        gethotelchainsids = 10,
+        getratebasisids = 11,
+        getairlinesids = 12,
+    }
+
+    public class DOTWAccess
+    {
+        private const string Url = "http://us.dotwconnect.com/gatewayV3.dotw";
+
+        private static string GeneralXmlReqByCommand(Command command) => $"<customer><username>Mehrdad</username><password>e3bb7ee8b41c04d9d94b82c74ee1325d</password><id>1080498</id><source>1</source><request command=\"{command.ToString()}\"></request></customer>";
+
+        private static string SearchHotelReqXml(int cityCode) => $"<customer><username>Mehrdad</username><password>e3bb7ee8b41c04d9d94b82c74ee1325d</password><id>1080498</id><source>1</source><product>hotel</product><request command=\"searchhotels\"><bookingDetails><fromDate>{DateTime.Now.Date.ToString("yyyy-MM-dd")}</fromDate><toDate>{DateTime.Now.AddDays(1).Date.ToString("yyyy-MM-dd")}</toDate><currency>520</currency><rooms no=\"1\"><room runno=\"0\"><adultsCode>1</adultsCode><children no=\"0\"></children><rateBasis>-1</rateBasis></room></rooms></bookingDetails><return><getRooms>true</getRooms><filters xmlns:a=\"http://us.dotwconnect.com/xsd/atomicCondition\" xmlns:c=\"http://us.dotwconnect.com/xsd/complexCondition\"><city>{cityCode}</city><noPrice>true</noPrice></filters><fields><field>priority</field></fields></return></request></customer>";
+
+        public static bool GetInternalCodes(Command command, out string result)
+        {
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    var content = new StringContent(GeneralXmlReqByCommand(command), Encoding.UTF8, "application/xml");
+                    var response = client.PostAsync(Url, content).Result;
+                    if (!response.IsSuccessStatusCode)
+                    {
+                        result = null;
+                        return false;
+                    }
+                    result = response.Content.ReadAsStringAsync().Result;
+                    return true;
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public static bool SearchHotelByCity(int cityCode, out string result)
+        {
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    var content = new StringContent(SearchHotelReqXml(cityCode), Encoding.UTF8, "application/xml");
+                    var response = client.PostAsync(Url, content).Result;
+                    if (!response.IsSuccessStatusCode)
+                    {
+                        result = null;
+                        return false;
+                    }
+                    result = response.Content.ReadAsStringAsync().Result;
+                    return true;
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public static bool SearchHotelByID(int ID, out string result)
+        {
+            try
+            {
+                throw new NotImplementedException();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
